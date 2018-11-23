4b333b35493903e7482b28a6d52eed3dbe9bd38c
Andrés Lancheros <alancherosr@gmail.com>
2018-11-23 16:27:05 -0300

    Init command

---
+++ backup_pg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Small script to take postgres backup by using docker container
+
+postgres_version=10
+postgres_username=postgres
+postgres_password=password
+postgres_host=127.0.0.1
+postgres_port=5432
+postgres_dbname=dbname
+
+docker run -it postgres:$postgres_version pg_dump --dbname=postgresql://$postgres_username:$postgres_password@$postgres_host:$postgres_port/$postgres_dbname | gzip > /home/$(postgres_dbname)_$(date +%Y-%m-%d).psql.gz
